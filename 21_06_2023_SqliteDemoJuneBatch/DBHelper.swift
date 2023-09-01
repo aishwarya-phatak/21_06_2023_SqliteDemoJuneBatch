57e9249311833bf83133dfaada2870e0a4cadfcd
aishwarya <ashp17star@gmail.com>
2023-09-01 13:39:58 +0530

    insert, retrive

--- 21_06_2023_SqliteDemoJuneBatch/DBHelper.swift
+++ 21_06_2023_SqliteDemoJuneBatch/DBHelper.swift
@@ -54,4 +54,74 @@ class DBHelper{
         }
         sqlite3_finalize(createTableStatement)
     }
+    
+    func insertPersonRecord(id : Int, name : String, age : Int){
+        let persons = retrivePersonRecords()
+        for eachPerson in persons{
+            if eachPerson.id == id{
+                return
+            }
+        }
+        
+        let insertStatementQueryString = "INSERT INTO Person(id,name,age) VALUES (?,?,?);"
+        
+        var insertStatement : OpaquePointer? = nil
+         if sqlite3_prepare_v2(
+            db,
+            insertStatementQueryString,
+            -1,
+            &insertStatement,
+            nil) == SQLITE_OK{
+             
+           sqlite3_bind_int(insertStatement, 1, Int32(id))
+           sqlite3_bind_text(
+                insertStatement,
+                2,
+                (name as NSString).utf8String,
+                -1,
+                nil)
+           sqlite3_bind_int(insertStatement, 3, Int32(age))
+            
+             if sqlite3_step(insertStatement) == SQLITE_DONE{
+                 print("The record is inserted successfully")
+             } else {
+                 print("Record insertion failed")
+             }
+         } else {
+             print("The insert statement preparation falied")
+         }
+        sqlite3_finalize(insertStatement)
+    }
+    
+    func retrivePersonRecords()->[Person]{
+        let retriveQueryString = "SELECT * FROM Person;"
+        var retriveStatment :OpaquePointer? = nil
+        var persons1 : [Person] = []
+        if sqlite3_prepare_v2(
+            db,
+            retriveQueryString,
+            -1,
+            &retriveStatment,
+            nil) == SQLITE_OK{
+            
+            while sqlite3_step(retriveStatment) == SQLITE_ROW{
+              let id = sqlite3_column_int(retriveStatment, 0)
+              let name = String(describing: String(cString: sqlite3_column_text(retriveStatment, 1)))
+              let year = sqlite3_column_int(retriveStatment, 2)
+                
+                let personObject = Person(
+                    name: name,
+                    id: Int(id),
+                    age: Int(year))
+                persons1.append(personObject)
+                
+//                print("Got the result of query")
+//                print("\(id) -- \(name)")
+            }
+        } else {
+            print("Insert statement preparation failed")
+        }
+        sqlite3_finalize(retriveStatment)
+        return persons1
+    }
 }
--- 21_06_2023_SqliteDemoJuneBatch/ViewController.swift
+++ 21_06_2023_SqliteDemoJuneBatch/ViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var persons : [Person] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var dbHelper = DBHelper()
+        print("The record insertion")
+        
+        dbHelper.insertPersonRecord(id: 10, name: "Tanaji", age: 28)
+        dbHelper.insertPersonRecord(id: 11, name: "Pratahmesh", age: 25)
+        dbHelper.insertPersonRecord(id: 12, name: "Sahil", age: 25)
+        
+        print("The record retrive method is called")
+        persons = dbHelper.retrivePersonRecords()
+        for eachPerson in persons{
+            print("\(eachPerson.id) -- \(eachPerson.name)")
+        }
     }
 
 
